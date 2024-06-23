ca00a39d6e22dbae25dadabf0131ad8bcfad0c2f
Amir Mohammad Khorasani <amirilf@protonmail.com>
2024-06-23 11:33:28 +0330

    Add auth fxml pages (auth,login,register) basics

--- client/src/main/java/app/client/gui/auth/Auth.java
+++ client/src/main/java/app/client/gui/auth/Auth.java
@@ -1,17 +1,22 @@
 package app.client.gui.auth;
 
+import app.client.gui.Utility;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class Auth {
 
+    @FXML
+    private Label lbl_login;
 
     @FXML
-    private void handleLoginButton(){
-        System.out.println("login clicked");
+    private void handleLoginButton() {
+        Utility.changeSceneofStage((Stage) lbl_login.getScene().getWindow(), "/app/client/fxml/auth/Login.fxml");
     }
 
     @FXML
-    private void handleRegisterButton(){
-        System.out.println("register clicked");
+    private void handleRegisterButton() {
+        Utility.changeSceneofStage((Stage) lbl_login.getScene().getWindow(), "/app/client/fxml/auth/Register.fxml");
     }
 }
--- client/src/main/java/app/client/gui/auth/Register.java
+++ client/src/main/java/app/client/gui/auth/Register.java
@@ -1,11 +1,37 @@
 package app.client.gui.auth;
 
+import app.client.gui.Utility;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Register {
 
     @FXML
-    private  void initializer(){
-        System.out.println("Login page is loaded!");
+    private TextField fld_name;
+
+    @FXML
+    private TextField fld_phone;
+
+    @FXML
+    private TextField fld_username;
+
+    @FXML
+    private TextField fld_password;
+
+    @FXML
+    private void handleRegisterButton() {
+        System.out.println("Register button clicked!");
     }
+
+    @FXML
+    private void handleLoginPageButton() {
+        Utility.changeSceneofStage((Stage) fld_name.getScene().getWindow(), "/app/client/fxml/auth/Login.fxml");
+    }
+
+    @FXML
+    private void handleAuthPageButton() {
+        Utility.changeSceneofStage((Stage) fld_name.getScene().getWindow(), "/app/client/fxml/auth/Auth.fxml");
+    }
+
 }
